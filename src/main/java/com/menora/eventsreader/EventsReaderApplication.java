be99a66e758b5dc2947d4b06b47882f6032e0c40
Yaniv P <yanivpartush@gmail.com>
2024-04-10 22:53:38 +0300

    Initial commit

---
+++ src/main/java/com/menora/eventsreader/EventsReaderApplication.java
@@ -0,0 +1,13 @@
+package com.menora.eventsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsReaderApplication.class, args);
+	}
+
+}
